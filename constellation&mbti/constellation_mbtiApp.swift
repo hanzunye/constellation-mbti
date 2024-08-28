da1d6a300b7cbf1794c9d6fb3dc30966b795bfa0
HAN <davongluck@gmail.com>
2024-08-28 16:07:43 +0200

    Initial commit

---
+++ constellation&mbti/constellation_mbtiApp.swift
@@ -0,0 +1,17 @@
+//
+//  constellation_mbtiApp.swift
+//  constellation&mbti
+//
+//  Created by HAN on 2024/8/28.
+//
+
+import SwiftUI
+
+@main
+struct constellation_mbtiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
